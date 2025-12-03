6dc5e2b9073df2d6db7b4f53737b012208a6de55
AKHIL PRATAP SINGH <as1575828@gmail.com>
2025-12-03 18:56:11 +0530

    Initialize touch sensor and buzzer in setup

--- TouchSensor.ino
+++ TouchSensor.ino
@@ -1,10 +1,12 @@
 int touchPin=7;
 int buzz=13;
+
 void setup(){
 pinMode(touchPin, INPUT);
 pinMode(buzz, OUTPUT);
 Serial.begin(9600);
 }
+
 void loop() {
   int touch=digitalRead(touchPin);
   if(touch == HIGH){
