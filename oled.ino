9fff8f94eda146218d7c57d397ae1d17040d652f
AKHIL PRATAP SINGH <as1575828@gmail.com>
2025-12-03 19:00:15 +0530

    Initial commit
    Removed unnecessary blank line in setup function.

--- oled.ino
+++ oled.ino
@@ -9,7 +9,6 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(9600);
-  
   // Initialize OLED display
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
