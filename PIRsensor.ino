855bed3f1dbd7789f8cd9a7573f01a0fac09b742
akhil-singh28 <as1575828@gmail.com>
2025-12-05 22:34:37 +0530

    Initial Commit

---
+++ PIRsensor.ino
@@ -0,0 +1,17 @@
+int pir = 3;
+void setup(){}
+pinMode(pir, INPUT);
+Serial.begin(9600);
+void loop()
+{
+    int pirValue = digitalRead(pir);
+    if (pirValue == 1)
+    {
+        Serial.println("Motion Detected");
+    }
+    else
+    {
+        Serial.println("No Motion Detected");
+    }
+    delay(1000);
+}
